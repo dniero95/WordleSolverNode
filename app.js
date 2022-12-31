1a9e215c2ceb648b35729fc2218cdd8c14ebbf56
dniero95 <darioniero@icloud.com>
2022-12-31 17:42:28 +0100

    create app.js; instantiete server

---
+++ app.js
@@ -0,0 +1,11 @@
+//  import module http
+ const http = require('http');
+
+
+ const server = http.createServer((req, res)=>{
+    console.log(req);
+ });
+
+ server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+  });
