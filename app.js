957f7baf2cd59f7a3004777e565b0c1d83e744c0
dniero95 <darioniero@icloud.com>
2022-12-31 18:50:16 +0100

    add form

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 //  import module http
- const http = require('http');
+const http = require('http');
 //  const fs = require('fs')
 //  const express = require('express');
 
- const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers);
-    // the following line close the program if executed
-    // process.exit();
-   
-    res.setHeader('Content-Type', 'text/html');
-    res.write('wordle solver');
-    res.end();
+const server = http.createServer((req, res) => {
 
- });
+   // follow code log in the console url, method and header attribute of the req obj
+   //  console.log(req.url, req.method, req.headers);
+   // the following line close the program if executed
+   // process.exit();
+   const url = req.url;
+   if (url === '/') {
+      res.write('<html>');
+      res.write('<head><title>home</title></head>');
+      res.write('<body><h1>wordle solver</h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+
+      res.write('</html>');
+      
+      return res.end();
+   }
+
+
+   res.setHeader('Content-Type', 'text/html');
+   res.write('wordle solver');
+   res.end();
+
+});
 
 
 
@@ -29,5 +42,5 @@
 //  });
 
 server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
-  });
+   console.log('listening for requests on port 3000');
+});
