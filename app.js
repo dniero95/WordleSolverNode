2ae3cd7a1fb0a0d12429695145b96f3206f49195
dniero95 <darioniero@icloud.com>
2022-12-31 20:13:10 +0100

    use async writeFile

--- app.js
+++ app.js
@@ -18,15 +18,27 @@ const server = http.createServer((req, res) => {
       res.write('<body><h1>wordle solver</h1><form action="/word" method="POST"><input type="text" name="word"><button type="submit">send</button></form></body>')
 
       res.write('</html>');
-      
+
       return res.end();
    }
 
-   if (url ==='/word' && method === 'POST'){
-      fs.writeFileSync('word.txt', 'lorem ipsum');
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      return res.end();
+   if (url === '/word' && method === 'POST') {
+      const body = [];
+      req.on('data', (chunk) => {
+         console.log(chunk);
+         body.push(chunk);
+      });
+      return req.on('end', () => {
+         const parsedBody = Buffer.concat(body).toString();
+         const message = parsedBody.split('=')[1];
+         fs.writeFile('word.txt', message, err=>{
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+         });
+      });
+
+      
    }
 
 
