8ae30455a0c68884fbb8d22bb6a584ab19b18af3
dniero95 <darioniero@icloud.com>
2023-01-02 17:47:07 +0100

    istantiete express in app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //  import module http
 const http = require('http');
+const express = require('express')
 const routes = require('./routes');
 
-const server = http.createServer(routes);
+
+const app = express();
+const server = http.createServer(app);
 
 server.listen(3000, 'localhost', () => {
    console.log('listening for requests on port 3000');
