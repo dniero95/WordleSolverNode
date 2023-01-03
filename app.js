fdf54a40402faa645c796ae29b9b05a8450b5388
dniero95 <darioniero@icloud.com>
2023-01-03 04:25:44 +0100

    add body-parser; add basic form

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 
 // import express
-const express = require('express')
-
+const express = require('express');
+const bodyParser = require('body-parser');
 // create the app
 const app = express();
 
 // routes
 
+// parser of the body
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/about', (req, res, next)=>{
    console.log('in the about middleware!');
-   res.send('<h1>About wordle solver</h1>')
+   res.send(`<h1>About wordle solver</h1>`);
 });
 app.use('/docs', (req, res, next)=>{
    console.log('in documentation middleware!');
    res.send('<h1>wordle solver docs</h1>')
 });
 
+app.use('/word', (req, res, next)=>{
+   console.log('in the /word middleware');
+   console.log(req.body);
+   res.redirect('/')
+});
+
 app.use('/', (req, res, next)=>{
    console.log('in home middleware!');
-   res.send('<h1>wordle solver</h1>')
+   res.send(`<h1>wordle solver</h1>
+   <form action="/word" method="POST">
+   <input type="text" name="word">
+   <button type="submit">submit</button>
+   </form>`)
 });
 
 
