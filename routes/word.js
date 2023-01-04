391aed1d3d92e723d0dcce4d1b693e87c93ec331
dniero95 <darioniero@icloud.com>
2023-01-04 18:46:36 +0100

    word now access all info for the filter

--- routes/word.js
+++ routes/word.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const rootDir = require('../util/path');
+
 
 
 router.post('/word', (req, res, next)=>{
     console.log('in the /word middleware');
     console.log(req.body);
     // fs.writeFile('../word.txt', req.body.word);
+    try {
+        var words = fs.readFileSync(path.join(rootDir,'words.txt'), 'utf8');
+      } catch (err) {
+        console.error(err);
+      }
+    words = words.split('\n');
+    console.log(words);
     res.redirect('/')
  });
 
