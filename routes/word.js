77a78797e6b2922acfebe16f5e741929fe7b2863
dniero95 <darioniero@icloud.com>
2023-01-04 11:43:32 +0100

    index.html now communicate with word.js middleware

--- routes/word.js
+++ routes/word.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/word', (req, res, next)=>{
     console.log('in the /word middleware');
-    console.log(req.body.word);
+    console.log(req.body);
     // fs.writeFile('../word.txt', req.body.word);
     res.redirect('/')
  });
