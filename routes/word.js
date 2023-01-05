128549087448f64f3ff91fbe34d5ce4a4c572c70
dniero95 <darioniero@icloud.com>
2023-01-05 19:31:33 +0100

    add filter util

--- routes/word.js
+++ routes/word.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path');
+const filter = require('../util/filter');
 
 
 
---
+++ util/filter.js
@@ -0,0 +1,14 @@
+const { words } = require("../routes/word");
+
+const filter = (randomLetters, wrongLetters, correctLetters) => {
+    for (let word in words) {
+        for (let index = 0; index < wrongLetters.length; index++) {
+            const letter = wrongLetters[index];
+            if (word.includes(letter)) {
+                
+            }  
+        }
+    }
+}
+
+module.exports = filter;
